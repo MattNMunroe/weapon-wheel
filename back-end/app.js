1a61752033bc32e022abba8550247ee95f34e2a4
Matt M <Afrodesiacc@gmail.com>
2022-08-24 12:54:35 -0400

    Fleshing out back-end.

---
+++ back-end/app.js
@@ -0,0 +1,24 @@
+// DEPENDENCIES
+const express = require("express");
+const cors = require("cors");
+const weaponsControllers = require("./controllers/weaponsController");
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+app.use("/weapons", weaponsControllers);
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Engarde!");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("This route doesn't exist");
+});
+
+// EXPORT
+module.exports = app;
